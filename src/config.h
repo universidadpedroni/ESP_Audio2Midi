c106ef5d4a4417ae4b9999f45bbf4a3cee8541ac
universidadpedroni <universidad.pedroni@gmail.com>
2023-06-23 21:41:17 -0300

    Se cambió analogRead por adc1_get_raw

--- src/config.h
+++ src/config.h
@@ -3,10 +3,10 @@
 const uint16_t SAMPLES = 1024; //This value MUST ALWAYS be a power of 2
 double vReal[SAMPLES];
 double vImag[SAMPLES];
-const double SAMPLING_FREQ = 10000; //12800;
+const double SAMPLING_FREQ = 1024.0 / 46.0e-3;// 24000;
 const int PIN_ADC = GPIO_NUM_36;
 
-const float MAG_2_MIDI = 127.0 / 600000.0; // Este valor depende de la frecuencia de muestreo.
+const float MAG_2_MIDI = 127.0 / 680000.0; // Este valor depende de la frecuencia de muestreo.
 
 const long BAUDRATE = 115200;       // Baudrte for serial com
 const long MIDI_BAUDRATE = 31250;    // Baudrate for MIDI port
--- src/main.cpp
+++ src/main.cpp
@@ -4,95 +4,58 @@
 #include "fftAux.h"
 #include "serialFunc.h"
 
-// WiFi and OTA
-#include "wifiFunctions.h"
+#include "esp_adc_cal.h"
 
-// RTOS
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
+
+#define DEFAULT_VREF    1100        // Valor de referencia por defecto en mV (para ADC de 12 bits)
+#define ADC_WIDTH       ADC_WIDTH_BIT_12
+#define ADC_ATTEN       ADC_ATTEN_DB_11
+
 
 fftAux FFTAux(SAMPLES, SAMPLING_FREQ);
 SerialFuncClass sf; // Create an instance of the serialFunc class
+
 nota_t nota = {0, 0, 0, 0};
 
-hw_timer_t *My_timer = NULL;
-bool adquisitionCompleted = false; 
-
-void serviciosWiFienCore1(void * params){
-    Serial.printf("Generando Servicios WiFi desde Core %d\n",xPortGetCoreID());
-    OverTheAirUpdate();
-    while(true){}
-}
-
-
-void IRAM_ATTR  onTimer()
-{
-    digitalWrite(GPIO_NUM_17, HIGH);
-    static int i = 0;
-    FFTAux.vReal[i] = static_cast<double>(analogRead(PIN_ADC));
-    if(i < SAMPLES){
-        i++;
-    }
-    else{
-        i = 0;
-        adquisitionCompleted = true;
-        timerAlarmDisable(My_timer);
-    }
-
-    digitalWrite(GPIO_NUM_17, LOW);
-}
-
-void timerInit(){
-    My_timer = timerBegin(0, 80, true);
-    timerAttachInterrupt(My_timer, &onTimer, true);
-    timerAlarmWrite(My_timer, 100, true);
-    timerAlarmEnable(My_timer);     
-}
-
 void Adquire(){
+    digitalWrite(GPIO_NUM_17, HIGH);
     for(int i = 0; i < SAMPLES; i++)
     {
-      FFTAux.vReal[i] = static_cast<double>(analogRead(PIN_ADC));
+      FFTAux.vReal[i] = static_cast<double>(adc1_get_raw(ADC1_CHANNEL_0));
       FFTAux.vImag[i] = 0;
     }
+    digitalWrite(GPIO_NUM_17, LOW);
 }
 
 void setup(){
     delay(2000);
     Serial.begin(BAUDRATE);
-    SerialMidi.begin(MIDI_BAUDRATE);
-    Serial.println("Ready");
+    //SerialMidi.begin(MIDI_BAUDRATE);
     pinMode(GPIO_NUM_17, OUTPUT);
-    digitalWrite(GPIO_NUM_17, HIGH);
-    xTaskCreatePinnedToCore(serviciosWiFienCore1, "WiFi en Core 1", 10000, NULL, 1, NULL, 1);
-    timerInit();
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN);
+    Serial.println("Ready");
+
     Serial.println("Setup finished!");
 }
 
 void loop(){
-    
-    //Adquire();
-    if(adquisitionCompleted){
-        FFTAux.DCRemoval();
-        FFTAux.ComputeFFT();
-        nota.peakFreq = FFTAux.FindPeak();
-        nota.Magnitude = MAG_2_MIDI * FFTAux.FindMag();
-        nota.notaMidi = FFTAux.findNote(nota.peakFreq);
-        
-        
 
-        if ((nota.notaMidi != nota.notaMidiOld) && (nota.Magnitude > MAGNITUDE_THRESHOLD)){
-            //if(Magnitude > MAGNITUDE_THRESHOLD) sf.sendMIDI(SerialMidi, NOTE_ON, 0, 127, MIDI_CHANNEL);
-            //if(Magnitude > MAGNITUDE_THRESHOLD) sf.serialInfo(peakFreq, Magnitude, notaMidi);
-            nota.notaMidiOld = nota.notaMidi;
-            //sf.serialSendFFT(FFTAux.vReal,SAMPLES);
-            sf.sendMIDI(SerialMidi, NOTE_ON, 0, 127, MIDI_CHANNEL);
-            sf.serialInfo(nota.peakFreq, nota.Magnitude, nota.notaMidi);
-        }
+    Adquire();
+    FFTAux.DCRemoval();
+    FFTAux.ComputeFFT();
+    nota.peakFreq = FFTAux.FindPeak();
+    nota.Magnitude = MAG_2_MIDI * FFTAux.FindMag();
+    nota.notaMidi = FFTAux.findNote(nota.peakFreq);
+    sf.serialInfo(nota.peakFreq, nota.Magnitude, nota.notaMidi, 2000);
 
-        adquisitionCompleted = false;
-        timerAlarmEnable(My_timer);
-        
+    if ((nota.notaMidi != nota.notaMidiOld) && (nota.Magnitude > MAGNITUDE_THRESHOLD)){
+        //if(Magnitude > MAGNITUDE_THRESHOLD) sf.sendMIDI(SerialMidi, NOTE_ON, 0, 127, MIDI_CHANNEL);
+        //if(Magnitude > MAGNITUDE_THRESHOLD) sf.serialInfo(peakFreq, Magnitude, notaMidi);
+        nota.notaMidiOld = nota.notaMidi;
+        //sf.serialSendFFT(FFTAux.vReal,SAMPLES);
+        //sf.sendMIDI(SerialMidi, NOTE_ON, 0, 127, MIDI_CHANNEL);
+        sf.serialInfo(nota.peakFreq, nota.Magnitude, nota.notaMidi);
     }
-     
+
 }
